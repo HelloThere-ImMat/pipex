205460ec1ba0935409ffd3c28e1e12135390b065
Mathias DORR <mdorr@e1r10p12.clusters.42paris.fr>
2023-03-05 11:53:42 +0100

    I think I fixed it

--- bonus_srcs/b_pipex.c
+++ bonus_srcs/b_pipex.c
@@ -6,7 +6,7 @@
 /*   By: mdorr <mdorr@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/29 16:00:48 by mdorr             #+#    #+#             */
-/*   Updated: 2023/03/03 15:28:37 by mdorr            ###   ########.fr       */
+/*   Updated: 2023/03/05 11:51:48 by mdorr            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,13 +26,22 @@ int	execute(char **command, char **path, char **env)
 	return (1);
 }
 
+void	free_pipes(t_data data)
+{
+	int	i;
+
+	i = 0;
+	while (data.end_tab[i])
+		free(data.end_tab[i++]);
+	free(data.end_tab);
+}
+
 void	pipex_mult(t_data data, char ***commands, char **path)
 {
 	int	i;
 
 	create_pipes(&data, commands, path);
 	i = 0;
-	printf("fd of end tab [7] is %d\n", data.end_tab[2][0]);
 	while (i < data.cmdnbr)
 	{
 		child_process(data, commands, path, i);
@@ -42,7 +51,7 @@ void	pipex_mult(t_data data, char ***commands, char **path)
 	close_pipes(&data);
 	if (data.heredoc == 1)
 		unlink(".heredoc_tmp");
-	free(data.end_tab);
+	free_pipes(data);
 	free_all(commands, path);
 }
 
@@ -73,6 +82,7 @@ int	main(int argc, char **argv, char **env)
 
 /*
 
-WE have DUP ERRORS WHEN ONE OF THE COMMANDS IS WRONG, IT IS PROBABLY LINKED TO THE COMMANDS TAB AND THE PIPE TAB WHICH SEG FAULTS CF LINE 35
+WE have DUP ERRORS WHEN ONE OF THE COMMANDS IS WRONG, IT IS PROBABLY LINKED
+TO THE COMMANDS TAB AND THE PIPE TAB WHICH SEG FAULTS CF LINE 35
 
 */
--- bonus_srcs/b_processes.c
+++ bonus_srcs/b_processes.c
@@ -6,7 +6,7 @@
 /*   By: mdorr <mdorr@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/01 18:08:02 by mdorr             #+#    #+#             */
-/*   Updated: 2023/03/03 15:19:32 by mdorr            ###   ########.fr       */
+/*   Updated: 2023/03/05 11:42:31 by mdorr            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,12 @@ void	sub_dup(int in, int out, char ***commands, char **path)
 	int	d1;
 	int	d2;
 
-	write(1, "in dup\n", 7);
 	d1 = dup2(in, STDIN_FILENO);
 	d2 = dup2(out, STDOUT_FILENO);
-	printf("D1 is %d and D2 is %d\n", d1, d2);
-	printf("in est %d et out est %d\n", in, out);
-	if (d1 < 0 || d2 < 0)
-		error(3, commands, path);
+	if (d1 < 0)
+		d1 = dup2(-1, STDIN_FILENO);
+	(void)commands;
+	(void)path;
 }
 
 void	close_pipes(t_data *data)
@@ -48,7 +47,7 @@ void	child_process(t_data data, char ***commands, char **path, int cmd_i)
 			sub_dup(data.in, data.end_tab[cmd_i][1], commands, path);
 		else if (cmd_i == data.cmdnbr - 1)
 			sub_dup(data.end_tab[cmd_i - 1][0], data.out, commands, path);
-		else
+		else if (cmd_i > 0 && cmd_i < data.cmdnbr - 1)
 			sub_dup(data.end_tab[cmd_i - 1][0],
 				data.end_tab[cmd_i][1], commands, path);
 		close_pipes(&data);
