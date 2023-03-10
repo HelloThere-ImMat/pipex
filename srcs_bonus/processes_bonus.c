2e29f94d81fba8425576dd5a136fca6b80fb7a97
Mathias DORR <mdorr@e3r9p14.clusters.42paris.fr>
2023-03-10 14:02:39 +0100

    fixed multiple process problem with the wrong commands

--- srcs_bonus/processes_bonus.c
+++ srcs_bonus/processes_bonus.c
@@ -6,7 +6,7 @@
 /*   By: mdorr <mdorr@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/01 18:08:02 by mdorr             #+#    #+#             */
-/*   Updated: 2023/03/08 18:14:57 by mdorr            ###   ########.fr       */
+/*   Updated: 2023/03/10 14:00:36 by mdorr            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,6 +43,8 @@ void	child_process(t_data data, char ***commands, char **path, int cmd_i)
 	data.pid = fork();
 	if (data.pid == 0)
 	{
+		if (!commands[cmd_i][0])
+			return ;
 		if (cmd_i == 0)
 			sub_dup(data.in, data.end_tab[cmd_i][1], commands, path);
 		else if (cmd_i == data.cmdnbr - 1)
@@ -51,8 +53,7 @@ void	child_process(t_data data, char ***commands, char **path, int cmd_i)
 			sub_dup(data.end_tab[cmd_i - 1][0],
 				data.end_tab[cmd_i][1], commands, path);
 		close_pipes(&data);
-		if (!commands[cmd_i][0])
-			return ;
-		execute(commands[cmd_i], path, data.env);
+		if (execute(commands[cmd_i], path, data.env) == 1)
+			error(0, commands, path);
 	}
 }
